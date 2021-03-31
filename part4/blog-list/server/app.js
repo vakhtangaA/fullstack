b2b8f49dce1d5af21781e3a5fb18b0c398eda03f
vakhtangaA <vakhtang.chitauri@gmail.com>
2021-03-31 22:58:02 +0400

    refactored application into modules

--- part4/blog-list/server/app.js
+++ part4/blog-list/server/app.js
@@ -1,44 +1,39 @@
-require("dotenv").config();
+const config = require("./utils/config");
 const path = require("path");
 const express = require("express");
+const blogRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
 const app = express();
+const logger = require("./utils/logger");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number,
-});
+logger.info("connecting to", config.MONGODB_URI);
 
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex: true,
-});
+const mongoUrl = config.MONGODB_URI;
+mongoose
+	.connect(mongoUrl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		logger.info("connected to MongoDB");
+	})
+	.catch(error => {
+		logger.error("error connecting to MongoDB: ", error.message);
+	});
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
+app.use(middleware.requestLogger);
 
-app.get("/api/blogs", (request, response) => {
-	Blog.find({}).then(blogs => {
-		response.json(blogs);
-	});
-});
+app.use("/api/blogs", blogRouter);
 
-app.post("/api/blogs", (request, response) => {
-	const blog = new Blog(request.body);
-
-	blog.save().then(result => {
-		response.status(201).json(result);
-	});
-});
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
